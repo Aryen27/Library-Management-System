6dd4516229bc86b2b9b2bd684d0a3594baae47c9
Aryan Rajeev <raghavaan27@gmail.com>
2025-05-10 04:06:30 +0530

    Added menu for MDP

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,27 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static int menu() {
+        System.out.println("Enter your choice");
+        System.out.println("-----------------------------------------------------------------");
+        System.out.println("0. EXIT");
+        System.out.println("1. Add Book");
+        System.out.println("4. Update Book");
+        System.out.println("5. Display all books in library");
+        System.out.println("6. Display all avaiable books in library");
+        System.out.println("-----------------------------------------------------------------");
+        Scanner sc= new Scanner(System.in);
+        int choice= sc.nextInt();
+        return choice;
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        int choice;
+        while((choice=menu())!=0) {
         }
     }
 }
