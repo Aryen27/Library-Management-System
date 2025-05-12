6b1c94c7640bb7f317554856d9f6efa0c7a7f7a4
Aryan Rajeev <raghavaan27@gmail.com>
2025-05-13 02:58:48 +0530

    Added accessor functions for Book

--- src/main/java/model/Book.java
+++ src/main/java/model/Book.java
@@ -6,4 +6,33 @@ public class Book {
     }
     int id, total_copies, available_copies;
     String book_name, description, author;
+
+    Book(int id, String book_name, String description, String author, int total_copies, int available_copies){
+        this.id=id;
+        this.book_name=book_name;
+        this.description= description;
+        this.author=author;
+        this.total_copies=total_copies;
+        this.available_copies=available_copies;
+    }
+
+    public String getName(){
+        return this.book_name;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public String getAuthor(){
+        return this.author;
+    }
+
+    public int getAvailable(){
+        return this.available_copies;
+    }
+
+    public int getTotalCopies(){
+        return this.total_copies;
+    }
 }
